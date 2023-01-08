f9080e39ba1d85c69f8d7325d296c2057d3b307a
Tri Le <tri.le@tyme.com>
2023-01-09 01:20:01 +0700

    Add States enum

--- src/main/kotlin/tri/le/statemachine/state/StateDefine.kt
+++ src/main/kotlin/tri/le/statemachine/state/StateDefine.kt
@@ -1,9 +1,24 @@
 package tri.le.statemachine.state
 
+
+enum class States(val isProcessing: Boolean = true) {
+  INIT,
+  INIT_FAILED(false),
+  RESERVE,
+  RESERVE_FAILED(false),
+  TRANSFER,
+  TRANSFER_FAILED(false),
+  SUCCESS(false),
+
+  // just default state
+  DUMMY(false),
+}
+
 data class State(
   val name: String,
   val allowRetry: Boolean = false,
 )
+
 data class Flow(
   var name: String,
   val states: Set<State>,
