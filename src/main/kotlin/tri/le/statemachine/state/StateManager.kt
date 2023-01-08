c2740ae114142f86b3332ac0b4b3943991eef2ec
Tri Le <tri.le@tyme.com>
2023-01-08 11:29:16 +0700

    Add StateManager

--- src/main/kotlin/tri/le/statemachine/state/StateManager.kt
+++ src/main/kotlin/tri/le/statemachine/state/StateManager.kt
@@ -1,12 +1,19 @@
 package tri.le.statemachine.state
 
 import org.springframework.stereotype.Component
-import tri.le.statemachine.uti.Logger
+import tri.le.statemachine.state.handler.InitTransactionHandler
+import tri.le.statemachine.transfer.TransferInfo
+import tri.le.statemachine.uti.Log
 
 @Component
-class StateManager : Logger {
+class StateManager : Log {
 
   fun handle() {
     l.info("Handle")
+
+    val handle = InitTransactionHandler()
+    val transferInfo = TransferInfo("20230108-0001", "Alex", "Blob", 10.toBigInteger())
+    handle.doHandle(transferInfo)
+    handle.doHandle(transferInfo, 1)
   }
 }
