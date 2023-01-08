ad90f8b62ff17480d4a4a5a7ed6ec1108b5b5a85
Tri Le <tri.le@tyme.com>
2023-01-09 01:18:43 +0700

    Update logic StateManager

--- src/main/kotlin/tri/le/statemachine/state/StateManager.kt
+++ src/main/kotlin/tri/le/statemachine/state/StateManager.kt
@@ -1,22 +1,34 @@
 package tri.le.statemachine.state
 
 import org.springframework.stereotype.Component
+import tri.le.statemachine.state.States.INIT
+import tri.le.statemachine.state.handler.DummyHandler
 import tri.le.statemachine.state.handler.StateHandler
 import tri.le.statemachine.transfer.TransferInfo
 import tri.le.statemachine.uti.Log
 
+
 @Component
 class StateManager(
-  handlers: List<StateHandler<TransferInfo>>
+  handlers: List<StateHandler<TransferInfo>>,
+  private val dummyHandler: DummyHandler
 ) : Log {
 
-  private val handlerMap = handlers.associateBy { it.name }
+  private val handlerMap = handlers.associateBy { it.state }
   fun handle() {
     l.info("Handle")
 
-    val handle = handlerMap["INIT_TRANSACTION"]
+    var handle = handlerMap[INIT]!!
     val transferInfo = TransferInfo("20230108-0001", "Alex", "Blob", 10.toBigInteger())
-    handle?.doHandle(transferInfo)
-    handle?.doHandle(transferInfo, 1)
+
+    while (true) {
+      val nextState = handle.doHandle(transferInfo)
+      l.info("State update from ${handle.state} to ${nextState}")
+      if (!(nextState.isProcessing)) {
+        break
+      }
+
+      handle = handlerMap.getOrDefault(nextState, dummyHandler)
+    }
   }
 }
