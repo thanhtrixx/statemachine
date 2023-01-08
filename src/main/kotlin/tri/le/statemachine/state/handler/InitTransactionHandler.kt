13108982626866c78d8ef39b61b4f4a535211f37
Tri Le <tri.le@tyme.com>
2023-01-09 01:20:33 +0700

    Update handle method base on StateHandler

--- src/main/kotlin/tri/le/statemachine/state/handler/InitTransactionHandler.kt
+++ src/main/kotlin/tri/le/statemachine/state/handler/InitTransactionHandler.kt
@@ -1,26 +1,27 @@
 package tri.le.statemachine.state.handler
 
-import io.micrometer.common.lang.NonNull
-import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Component
-import tri.le.statemachine.state.StateManager
+import tri.le.statemachine.state.States
+import tri.le.statemachine.state.States.*
 import tri.le.statemachine.transfer.TransferInfo
 import tri.le.statemachine.uti.Log
 
 @Component
-class InitTransactionHandler(
-  @NonNull @Lazy
-  val stateManager: StateManager
-) : StateHandler<TransferInfo>(), Log {
+class InitTransactionHandler : StateHandler<TransferInfo>(), Log {
 
-  override val name = "INIT_TRANSACTION"
-  override fun handle(transferInfo: TransferInfo) {
+  override val state = INIT
+
+  override val checkToHandle = retryMaxAttempts(3)
+
+  override val errorState = INIT_FAILED
+
+  override fun handle(transferInfo: TransferInfo): States {
     l.info { "Init transaction $transferInfo" }
+    l.info { "Save transaction" }
+
+    return RESERVE
   }
 
-  override val checkToHandle = notRetry
-
-  override val errorState = "INIT_FAILED"
   override fun handleError(data: TransferInfo, executedCount: Int) {
     l.info("Handling error")
   }
--- src/main/kotlin/tri/le/statemachine/state/handler/ReserveHandler.kt
+++ src/main/kotlin/tri/le/statemachine/state/handler/ReserveHandler.kt
@@ -1,4 +1,27 @@
 package tri.le.statemachine.state.handler
 
-class ReserveHandler {
+import org.springframework.stereotype.Component
+import tri.le.statemachine.state.States
+import tri.le.statemachine.state.States.*
+import tri.le.statemachine.transfer.TransferInfo
+import tri.le.statemachine.uti.Log
+
+@Component
+class ReserveHandler : StateHandler<TransferInfo>(), Log {
+
+  override val state = RESERVE
+
+  override val checkToHandle = notRetry
+
+  override val errorState = RESERVE_FAILED
+
+  override fun handleError(data: TransferInfo, executedCount: Int) {
+    l.info("End transaction. Save transaction information into database with state")
+  }
+
+  override fun handle(data: TransferInfo): States {
+    l.info("Reserve transaction")
+
+    return TRANSFER
+  }
 }
