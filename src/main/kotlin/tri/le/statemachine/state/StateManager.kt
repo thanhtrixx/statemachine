040860218d29be65daabae0cc925b8184fe51fb3
Tri Le <tri.le@tyme.com>
2023-01-11 21:57:27 +0700

    Implement StateManager with threadPool & scheduledThreadPool

--- src/main/kotlin/tri/le/statemachine/state/StateManager.kt
+++ src/main/kotlin/tri/le/statemachine/state/StateManager.kt
@@ -1,34 +1,57 @@
 package tri.le.statemachine.state
 
+import org.apache.logging.log4j.ThreadContext
 import org.springframework.stereotype.Component
-import tri.le.statemachine.state.States.INIT
-import tri.le.statemachine.state.handler.DummyHandler
-import tri.le.statemachine.state.handler.StateHandler
-import tri.le.statemachine.transfer.TransferInfo
+import tri.le.statemachine.base.Traceable
+import tri.le.statemachine.state.handler.base.NextAction
+import tri.le.statemachine.state.handler.base.NoOpHandler
+import tri.le.statemachine.state.handler.base.StateHandler
 import tri.le.statemachine.uti.Log
-
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @Component
-class StateManager(
-  handlers: List<StateHandler<TransferInfo>>,
-  private val dummyHandler: DummyHandler
+class StateManager<D : Traceable>(
+  handlers: List<StateHandler<D>>, private val noOpHandler: NoOpHandler<D>,
 ) : Log {
 
   private val handlerMap = handlers.associateBy { it.state }
-  fun handle() {
-    l.info("Handle")
 
-    var handle = handlerMap[INIT]!!
-    val transferInfo = TransferInfo("20230108-0001", "Alex", "Blob", 10.toBigInteger())
+  private val threadPool = Executors.newFixedThreadPool(128)
+  private val scheduledThreadPool = Executors.newScheduledThreadPool(16)
 
-    while (true) {
-      val nextState = handle.doHandle(transferInfo)
-      l.info("State update from ${handle.state} to ${nextState}")
-      if (!(nextState.isProcessing)) {
-        break
+  fun handle(data: D, nextAction: NextAction) {
+    l.info("Begin handling flow traceId: ${data.traceId}")
+
+    val state = nextAction.state
+
+    if (!state.isProcessing) {
+      l.info("Complete flow: $nextAction")
+      return
+    }
+
+    when {
+      nextAction.delayMillis > 0 -> {
+        l.info("Schedule next action. Start processing in next ${nextAction.delayMillis}")
+        scheduledThreadPool.schedule(
+          { doHandle(data, state) },
+          nextAction.delayMillis.toLong(),
+          TimeUnit.MICROSECONDS
+        )
       }
 
-      handle = handlerMap.getOrDefault(nextState, dummyHandler)
+      nextAction.submitNewThread -> threadPool.submit { doHandle(data, state) }
+
+      else -> doHandle(data, state)
     }
+  }
+
+  private fun doHandle(data: D, state: States) {
+    ThreadContext.put("traceId", data.traceId)
+
+    val handler = handlerMap.getOrDefault(state, noOpHandler)
+    val nextAction = handler.handle(data, 0)
+    handle(data, nextAction)
+
   }
 }
