c62ac3d914c51955220b1f1235f190b663160ad5
Tri Le <tri.le@tyme.com>
2023-01-14 19:44:36 +0700

    Add NextAction.attemptedTimes

--- src/main/kotlin/tri/le/statemachine/state/handler/base/NextAction.kt
+++ src/main/kotlin/tri/le/statemachine/state/handler/base/NextAction.kt
@@ -5,6 +5,7 @@ import tri.le.statemachine.state.States
 data class NextAction(
   val state: States,
   val delayMillis: Int = 0,
+  val attemptedTimes: Int = 0,
   val submitNewThread: Boolean = false
 ) {
   companion object {
